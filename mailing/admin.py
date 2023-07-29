b5d453e94d622e0948b1c9e0563adbcf361f127f
Sandi_Navoiy <santes.au@gmail.com>
2023-07-29 16:26:07 +0300

    настойки рассылки

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from django.contrib.auth import admin
 
-# Register your models here.
+
+@admin.register(MailingSettings)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('mailing_status', 'mailing_time_start', 'mailing_time_start', 'mailing_periods',)
+    list_filter = ('mailing_status',)
+    search_fields = ('mailing_status',)
+
+
+@admin.register(Mail)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('settings', 'mailing_subject', 'mailing_body')
+    list_filter = ('settings',)
+    search_fields = ('settings',)
